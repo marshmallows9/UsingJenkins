0103d7bdf1e286f5facac303014558065f3b6ae1
Manas Sethi <manas_ranjan@apple.com>
2018-02-16 15:24:47 -0800

    SearchAutomate first commit

--- src/main/java/SearchAutomate/googleSearch.java
+++ src/main/java/SearchAutomate/googleSearch.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class googleSearch {
 
     public static void main(String[] args) {
-       // System.setProperty("webdriver.chrome.driver","/Users/manas/Downloads/chromedriver");
+        System.setProperty("webdriver.chrome.driver","/Users/manas/Downloads/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.get("http://google.com");
 
